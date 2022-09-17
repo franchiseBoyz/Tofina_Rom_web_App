44c017559826218473279c9fe8be25e4d92c5c4d
troye <troyegibert@pm.me>
2022-09-17 15:14:44 +0300

    updated Property.jsx

--- components/Property.jsx
+++ components/Property.jsx
@@ -12,7 +12,7 @@ const Property = ({property: { coverPhoto, price, rentFrequency, rooms, title, b
     <Link href={`/property/${externalID}`} passHref>
         <Flex flexWrap='wrap'  w='420px' p ='5' paddingTop='0' justifyContent='flex-start' cursor='pointer'>
             <Box>
-                <Image src = {coverPhoto ? coverPhoto.url : defaultImage } width={400} height={260} alt='house' />
+                <Image src = {coverPhoto ? coverPhoto.url : DefaultImage } width={400} height={260} alt='home' />
             </Box>
             <Box w="full">
                 <Flex paddingTop='2' alignItems="center" justifyContent="space-between">
